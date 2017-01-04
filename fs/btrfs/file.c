05785b659743d5c7abed038c709ff11fe26372be
Jeff Mahoney <jeffm@suse.com>
2017-01-04 13:27:19 -0500

    btrfs: extent-tree: Switch to new delalloc space reserve and release (bsc#963193).
    suse-commit: eade0e67105da50cb36463d87d3df7856022293f

--- fs/btrfs/file.c
+++ fs/btrfs/file.c
@@ -1618,7 +1618,7 @@ again:
 				btrfs_delalloc_release_metadata(inode,
 								release_bytes);
 			else
-				btrfs_delalloc_release_space(inode,
+				__btrfs_delalloc_release_space(inode, pos,
 							     release_bytes);
 		}
 
@@ -1671,7 +1671,8 @@ again:
 			btrfs_end_write_no_snapshoting(root);
 			btrfs_delalloc_release_metadata(inode, release_bytes);
 		} else {
-			btrfs_delalloc_release_space(inode, release_bytes);
+			__btrfs_delalloc_release_space(inode, pos,
+						       release_bytes);
 		}
 	}
 
